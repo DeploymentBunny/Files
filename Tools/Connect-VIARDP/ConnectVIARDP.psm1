cb2925702cab2b15312596910b5e880f9ed365e2
Mikael Nystrom <deploymentbunny@outlook.com>
2016-11-25 16:55:39 +0100

    Added ConnectVIARDP.psm1

---
+++ Tools/Connect-VIARDP/ConnectVIARDP.psm1
@@ -0,0 +1,39 @@
+﻿Function Connect-VIARDP {
+    <#
+    .Synopsis
+        Connect-VIARDP
+    .DESCRIPTION
+        Connect-VIARDP
+    .EXAMPLE
+        Connect-VIARDP -Connection SERVER01
+    .NOTES
+        Created:	 July 15, 2016
+        Version:	 1.0
+    
+        Updated:     Nov 25, 2016
+        Version:     1.1
+
+        Author - Mikael Nystrom
+        Twitter: @mikael_nystrom
+        Blog   : http://deploymentbunny.com
+
+        Disclaimer:
+        This script is provided 'AS IS' with no warranties, confers no rights and 
+        is not supported by the author.
+    .LINK
+        http://www.deploymentbunny.com
+    #>
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Connection
+    )
+    do
+    {
+        $ConTest = (Test-NetConnection -ComputerName $Connection -CommonTCPPort RDP).TcpTestSucceeded
+    }
+    until ($ConTest -eq "True")
+    mstsc.exe /v:$Connection
+}
