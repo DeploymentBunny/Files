acea0657e95a455bfd80614ab22a1d9da36169f5
Mikael Nystrom <deploymentbunny@outlook.com>
2017-04-02 05:31:26 +0200

    Update

---
+++ Tools/Create-ADUserDemo/ImportUsers.ps1
@@ -0,0 +1,8 @@
+﻿#Read data from Bootstrap XML
+$settings = "C:\setup\Files\Tools\Create-ADUserDemo\settings.xml"
+[xml]$settings = Get-Content $BootstrapFile -ErrorAction Stop
+
+$Users = $settings.Settings.Users.User | Where-Object -Property Active -EQ $True
+Foreach($User in $Users){
+    $User
+}
