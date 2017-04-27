475ec6a05e42b7ff270edaf44bc0459e1a04bc3e
Mikael Nystrom <deploymentbunny@outlook.com>
2017-04-28 01:25:04 +0200

    Added

---
+++ Tools/Start-VIADeDupJob/Start-VIADeDupJob.ps1
@@ -0,0 +1,19 @@
+﻿Function Wait-VIADedupJob
+{
+    while ((Get-DedupJob).count -ne 0 )
+    {
+        Get-DedupJob
+        Start-Sleep -Seconds 30
+    }
+}
+
+foreach($item in Get-DedupVolume){
+    Wait-VIADedupJob
+    $item | Start-DedupJob -Type Optimization -Priority High -Memory 80
+    Wait-VIADedupJob
+    $item | Start-DedupJob -Type GarbageCollection -Priority High -Memory 80 -Full
+    Wait-VIADedupJob
+    $item | Start-DedupJob -Type Scrubbing -Priority High -Memory 80 -Full
+    Wait-VIADedupJob
+}
+Get-DedupStatus
