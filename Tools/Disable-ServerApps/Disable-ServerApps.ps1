69f7331b9c477e58ccb91a92f222d1f926d6d7c7
Mikael Nystrom <deploymentbunny@outlook.com>
2017-10-10 17:53:23 +0200

    Added new Script

---
+++ Tools/Disable-ServerApps/Disable-ServerApps.ps1
@@ -0,0 +1,6 @@
+﻿$Services = 'CDPUserSvc','MapsBroker','PcaSvc','ShellHWDetection','OneSyncSvc','WpnService'
+
+foreach($Service in $Services){
+    Stop-Service -Name $Service -PassThru -Verbose | Set-Service -StartupType Disabled -Verbose
+}
+
