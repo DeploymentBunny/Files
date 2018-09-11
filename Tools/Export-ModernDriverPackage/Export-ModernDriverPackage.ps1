5b55eb9993fa95a27c9e97e0136f81fb97e186e3
Mikael Nystrom <deploymentbunny@outlook.com>
2018-09-11 22:23:46 +0200

    Add tools for Modern Driver Managment

---
+++ Tools/Export-ModernDriverPackage/Export-ModernDriverPackage.ps1
@@ -0,0 +1,60 @@
+﻿<#
+ # This script exports drivers from the machine you are running on, stores them in a folder and create a .CSV file that can be used to
+ # import custom driver package for the Modern Driver Managment tool. http://www.scconfigmgr.com/modern-driver-management/
+#>
+
+Function Export-TSxWindowsDriver {
+    Param(
+        $Path,
+        $Platform,
+        $WindowsVersion,
+        $Architecture,
+        $Version
+    )
+    #ExportDriver
+    $Make = (Get-WmiObject -Class Win32_Computersystem).Manufacturer
+    switch ($Make)
+    {
+        'HP'{
+            $MakeAlias='Hewlett-Packard'
+            $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystem).model
+            $BaseBoard = (Get-CIMInstance -ClassName MS_SystemInformation -NameSpace root\WMI).BaseBoardProduct
+        }
+    
+        'Hewlett-Packard'{
+            $MakeAlias=$Make
+            $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystem).model
+            $BaseBoard = (Get-CIMInstance -ClassName MS_SystemInformation -NameSpace root\WMI).BaseBoardProduct
+        }
+    
+        'LENOVO'{
+            $MakeAlias=$Make
+            $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystemProduct).version
+            $BaseBoard = ((Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Model).SubString(0, 4)).Trim()
+        }
+
+        'Microsoft Corporation'{
+            $MakeAlias='Microsoft'
+            $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystem).model
+            $BaseBoard = (Get-WmiObject -Class Win32_ComputerSystem).model
+        }
+        Default {}
+    }
+
+    New-Item -Path $Path -ItemType Directory -Force -ErrorAction Stop
+    New-Item -Path $Path\$MakeAlias -ItemType Directory -Force -ErrorAction Stop
+    New-Item -Path $Path\$MakeAlias\$ModelAlias -ItemType Directory -Force -ErrorAction Stop
+    if((Test-Path -Path $Path) -ne $true){"Unable to access exportpath"}
+    
+    Export-WindowsDriver -Online -Destination $Path\$MakeAlias\$ModelAlias
+    $items = Get-ChildItem -Path $Path\$MakeAlias\$ModelAlias -Filter PRN*
+    foreach ($item in $items){
+        Remove-Item -Path $item.fullname -Recurse -Force
+    }
+
+    $SourceDirectory = "$Path\$MakeAlias\$ModelAlias"
+    Set-Content -Path $("$Path\$MakeAlias\$ModelAlias.csv") -Value "Make,Model,Baseboard,Platform,Operating System,Architecture,Version,Source Directory"
+    Add-Content -Path $("$Path\$MakeAlias\$ModelAlias.csv") -Value "$MakeAlias,$ModelAlias,$BaseBoard,$Platform,$WindowsVersion,$Architecture,$Version,$SourceDirectory"
+}
+
+Export-TSxWindowsDriver -Path \\SCCM\Drivers$\W101709 -Platform ConfigMgr -WindowsVersion "Windows 10" -Version '1.0' -Architecture x64
---
+++ Tools/Test-ModernDriverPackage/Test-ModernDriverPackage.ps1
@@ -0,0 +1,50 @@
+﻿<#
+ # This script will get driver packages using the Modern Driver Managment WebServices on the machine you run it on
+ # (http://www.scconfigmgr.com/modern-driver-management)
+#>
+
+$Make = (Get-WmiObject -Class Win32_Computersystem).Manufacturer
+switch ($Make)
+{
+    'HP'{
+        $MakeAlias='Hewlett-Packard'
+        $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystem).model
+        $BaseBoard = (Get-CIMInstance -ClassName MS_SystemInformation -NameSpace root\WMI).BaseBoardProduct
+    }
+    
+    'Hewlett-Packard'{
+        $MakeAlias=$Make
+        $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystem).model
+        $BaseBoard = (Get-CIMInstance -ClassName MS_SystemInformation -NameSpace root\WMI).BaseBoardProduct
+    }
+
+    'Dell'{
+        $MakeAlias=$Make
+        $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystem).model
+        $BaseBoard = (Get-CIMInstance -ClassName MS_SystemInformation -NameSpace root\WMI).SystemSku
+    }
+
+    'LENOVO'{
+        $MakeAlias=$Make
+        $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystemProduct).version
+        $BaseBoard = ((Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty Model).SubString(0, 4)).Trim()
+    }
+
+    'Microsoft Corporation'{
+        $MakeAlias='Microsoft'
+        $ModelAlias = (Get-WmiObject -Class Win32_ComputerSystem).model
+        $BaseBoard = (Get-WmiObject -Class Win32_ComputerSystem).model
+    }
+    Default {}
+}
+
+$MakeAlias
+$ModelAlias
+$BaseBoard
+
+$SecrectKey = "329b3c28-1b52-4cd7-abe1-81d93d1e1dda" 
+$URI = "http://cm01.corp.viamonstra.com/ConfigMgrWebService/ConfigMgr.asmx" 
+$Web = New-WebServiceProxy -Uri $URI
+$Web.
+$result = $Web.GetCMPackage($SecrectKey,'Driver')
+$result | Where-Object PackageDescription -Like *$BaseBoard*
