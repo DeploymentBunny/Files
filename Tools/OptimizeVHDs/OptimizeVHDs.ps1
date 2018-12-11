8fb21cae90891aff9c415ef7f1f6c5be54521c40
Mikael Nystrom <deploymentbunny@outlook.com>
2018-12-11 21:13:52 +0100

    Update OptVHD, Minor stuff

--- Tools/OptimizeVHDs/OptimizeVHDs.ps1
+++ Tools/OptimizeVHDs/OptimizeVHDs.ps1
@@ -3,22 +3,26 @@ Param(
     $VMnames
 )
 
+if($VMnames -eq $null){
+    $VMnames = (Get-VM).Name
+}
+
 foreach($VMname in $VMnames){
     #Check if VM is running
-    Write-Verbose "Checking $VMname"
+    Write-Host "Checking $VMname"
     if((Get-VM -Name $VMname).State -eq "off" -and (Get-VM -Name $VMname).ParentCheckpointId -eq $null){
     
     #Find the disks
     foreach($VHD in ((Get-VMHardDiskDrive -VMName $VMname).Path)){
-        Write-Verbose "Working on $VHD, please wait"
-        Write-Verbose "Current size $([math]::truncate($(Get-VHD -Path $VHD).FileSize/ 1GB)) GB"
+        Write-Host "Working on $VHD, please wait"
+        Write-Host "Current size $([math]::truncate($(Get-VHD -Path $VHD).FileSize/ 1GB)) GB"
         Mount-VHD -Path $VHD -NoDriveLetter -ReadOnly
         Optimize-VHD -Path $VHD -Mode Full
-        Write-Verbose "Optimize size $([math]::truncate($(Get-VHD -Path $VHD).FileSize/ 1GB)) GB"
+        Write-Host "Optimize size $([math]::truncate($(Get-VHD -Path $VHD).FileSize/ 1GB)) GB"
         Dismount-VHD -Path $VHD
-        Write-Verbose ""
+        Write-Host ""
         }
     }
     else{Write-Warning "$VMname is not turned off or has a snapshot, will not be fixed"
-    Write-Verbose ""}
+    Write-Host ""}
 }
